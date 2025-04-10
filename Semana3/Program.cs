73ea77a7f81f6ebbb83964ada2429c40d779058d
Sleetify <unemaildeprueba@gmail.com>
2025-04-10 12:16:17 -0500

    ejercicio4

--- Semana3/Program.cs
+++ Semana3/Program.cs
@@ -23,5 +23,23 @@ namespace Semana3
         {
             Console.WriteLine("Jaren\n19\nIng. Sistemas");
         }
+
+        static void ejercicio4()
+        {
+            int num1, num2;
+
+            Console.Write("Ingrese el primer numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo numero: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("La suma es: " + (num1 + num2));
+            Console.WriteLine("La resta es: " + (num1 - num2));
+            Console.WriteLine("La multiplicacion es: " + (num1 * num2));
+            Console.WriteLine("La division es: " + (num1 / num2));
+            Console.WriteLine("El módulo es: " + (num1 % num2));
+        }
     }
 }
