8bc600bedf24f9967acaa16ffd6acce14d25a728
Sleetify <unemaildeprueba@gmail.com>
2025-04-10 11:50:30 -0500

    ejercicio1

---
+++ Semana3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine(nombre + " Bienvenido al curso de Fundamentos de Algoritmo");
+
+            Console.ReadKey();
+        }
+    }
+}
